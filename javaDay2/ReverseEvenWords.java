140710f3934d0d2bd58b55d791bf861c02627d75
AvinashDurai <61369353+AvinashDurai@users.noreply.github.com>
2020-05-15 17:43:38 +0530

    Add files via upload

---
+++ javaDay2/ReverseEvenWords.java
@@ -0,0 +1,25 @@
+package javaDay2;
+
+public class ReverseEvenWords {
+
+	public static void main(String[] args) {
+		
+		String input = "When the world realise its own mistake, corona will dissolve automatically";
+		String[] splitInput = input.split(" ");
+		for (int i = 0; i < splitInput.length; i++) 
+		{
+			if(i%2 != 0)
+			{
+				StringBuffer buffer = new StringBuffer(splitInput[i]);
+				splitInput[i] = buffer.reverse().toString();
+			}
+		}
+		
+		for (int i = 0; i < splitInput.length; i++) 
+		{
+			System.out.print(splitInput[i]+" ");
+		}
+
+	}
+
+}
---
+++ javaDay2/Sumof50numbers.java
@@ -0,0 +1,13 @@
+package javaDay2;
+
+public class Sumof50numbers {
+
+	public static void main(String[] args) {
+		int i,sum=0;
+		for(i=0;i<=50;i++) {
+			sum=sum+i;
+		}
+System.out.println("Sum of 50 numbers is : " + sum);
+	}
+
+}
