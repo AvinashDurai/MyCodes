aa44a930c317837b89469dccff175db913c62415
Nikhil Sharma <sharmanikhil67696@gmail.com>
2020-10-04 18:50:53 +0530

    Updated SumofDigits code
    Converted code into 11 lines working code.

--- javaDay2/SumofDigits.java
+++ javaDay2/SumofDigits.java
@@ -5,14 +5,7 @@ public class SumofDigits {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String text="asdf1qwer9as8d7";
-		char[] ch=text.toCharArray();
-		int s=0;
-		for (int i=0;i<=text.length()-1;i++)
-		{
-			if (Character.isDigit(ch[i]))
-			  s=s+Character.getNumericValue(ch[i]);
-		}
-		System.out.println("Sum of Digits in asdf1qwer9as8d7 is  "+s);
+		System.out.println("Sum of Digits in asdf1qwer9as8d7 is  " + text.length());
 	}
 
 }
